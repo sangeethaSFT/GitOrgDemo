5649c930247c3bdcae7b1ad6eec0d13adf0b098d
USER <USER@192.168.100.193>
2019-12-09 17:05:25 +0530

    Multiplication table

---
+++ GitOrgDemo/src/MultiplicationTable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class MultiplicationTable
+{
+
+	public static void main(String[] args) 
+	{
+		int a;
+
+	    System.out.println("Enter range of numbers to print their multiplication tables");
+	    Scanner in = new Scanner(System.in);
+
+	    a = in.nextInt();
+	   
+	    System.out.println("Multiplication table of "+a);
+
+	    for (int i = 1; i <= 10; i++)
+	    {
+	    	System.out.println(a+"*"+i+" = "+(a*i));
+	    }
+	   
+	}
+
+}
