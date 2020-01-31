64ea34b27c3a3df84bd03ddf75843c4090582c72
HaroonRasheedMS <msharoonrasheed@gmail.com>
2020-01-31 07:39:42 -0500

    Modified by Haroon

--- GitOrgDemo/src/MultiplicationTable.java
+++ GitOrgDemo/src/MultiplicationTable.java
@@ -6,7 +6,7 @@ public class MultiplicationTable
 	{
 		int a;
 
-
+		System.out.println("Multiplication table");
 	    System.out.println("Enter range of numbers to print their multiplication tables .. ");
 
 	    Scanner in = new Scanner(System.in);
